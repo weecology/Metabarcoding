0e68831f837c2e8f5e1a11cb25e78797c78a49d7
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2017-02-22 08:28:55 -0500

    script for plots from October data

---
+++ make_some_plots.R
@@ -0,0 +1,12 @@
+# Make plots with both ITS and trnL data
+# Feb 22, 2017
+
+########################
+# SOURCES
+
+setwd("C:/Users/ellen.bledsoe/Desktop/Git/Metagenomics")
+source("./trnL_data_prep.R")
+source("./ITS2_data_prep.R")
+source("./quick_plotting_functions.R")
+
+
