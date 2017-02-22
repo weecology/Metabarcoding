97300a16b2e57bb88ed6539e0148ce2b31ba5ff2
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2017-02-22 09:23:22 -0500

    add code for plotting both trnL and ITS data

--- make_some_plots.R
+++ make_some_plots.R
@@ -9,4 +9,36 @@ source("./trnL_data_prep.R")
 source("./ITS2_data_prep.R")
 source("./quick_plotting_functions.R")
 
+########################
+# PLOTS
 
+### trnL
+
+# rank abundance 
+rank_abundance(samples, reads, sp = c('PP', 'DM', 'DO')) 
+rank_abundance(samples, reads, sp = 'PP')
+rank_abundance(samples, reads, sp = c('DM', 'DO'))
+
+# sum by family
+sum_by_family(taxa_trnL, samples, reads)
+sum_by_family(taxa_trnL, samples, reads, sp = 'PP')
+sum_by_family(taxa_trnL, samples, reads, sp = c('DM', 'DO'))
+
+# by individual
+trap_vs_fresh_indv(samples, reads)
+
+
+### ITS
+
+# rank abundance 
+rank_abundance(samples, all_ITS, cut_off = 25) 
+rank_abundance(samples, all_ITS, sp = 'PP', cut_off = 25)
+rank_abundance(samples, all_ITS, sp = c('DM', 'DO'), cut_off = 25)
+
+# sum by family
+sum_by_family(taxa_its, samples, all_ITS)
+sum_by_family(taxa_its, samples, all_ITS, sp = 'PP')
+sum_by_family(taxa_its, samples, all_ITS, sp = c('DM', 'DO'))
+
+# by individual
+trap_vs_fresh_indv(samples, all_ITS, cut_off = 100)
