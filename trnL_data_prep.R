f3fa094f0bc896a7a1825d4b366d5755e5265f3d
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2017-05-21 17:26:53 -0400

    add code for trnL link file and remove code for trnL voucher link file

--- trnL_data_prep.R
+++ trnL_data_prep.R
@@ -12,27 +12,44 @@ library(ggplot2)
 ########################
 # LOAD FILES
 
-trnL <- read.csv("./Plants/trnL_fecal_samples.csv", header = TRUE)
-samples <- read.csv("./CollectionData/fecal_sample_collection.csv", header = TRUE)
+setwd("~bleds22e/Documents/Git/Metagenomics/")
 
-########################
-# CLEAN DATA
+trnL_fecal <- read.csv("./Plants/trnL_fecal_samples.csv", header = TRUE)
+fecal <- read.csv("./CollectionData/fecal_sample_collection.csv", header = TRUE)
+trnL_voucher <- read.csv("./Plants/trnL_voucher_specimens.csv", header = TRUE)
+voucher <- read.csv("./CollectionData/plant_voucher_collection.csv", header = TRUE)
+trnL_link_file <- read.csv("./Plants/trnL_link_file.csv", header = TRUE)
 
-trnL <- trnL[-(c(452:453)),]
+#=====================================================================================
+### FULL trnL LINK FILE ###
 
-# taxonomy dataframe
-
-taxa_trnL <- select(trnL, OTU_ID, ConsensusLineage) %>% 
-  rename(OTU.ID = OTU_ID)
+# rename columns
+colnames(trnL_link_file) <- c("OTU_cluster", "Type", "Taxa")
 
 for(this_level in c('d','k','p','c','o','f','g','s')){
   # separate taxa into columns
-  step_one=sapply(strsplit(as.character(taxa_trnL$ConsensusLineage), paste0(this_level,'__')), '[', 2)
-  step_two=sapply(strsplit(step_one, ';'), '[', 1)
-  taxa_trnL[,this_level]=step_two
+  step_one = sapply(strsplit(as.character(trnL_link_file$Taxa), paste0(this_level,':')), '[', 2)
+  step_two = sapply(strsplit(step_one, ','), '[', 1)
+  trnL_link_file[ ,this_level] = step_two
 }
 
-taxa_trnL <- rename(taxa_trnL, Family = o, Genus = g, Species = s)
+# remove trailing semicolon from species column
+trnL_link_file$s = sapply(strsplit(trnL_link_file$s, ';'), '[', 1)
+
+# rename columns
+trnL_link_file <- select(trnL_link_file, OTU_cluster, Type, d, k, p, c, o, f, g, s) %>% 
+  rename(Family = o, Genus = g, SciName = s)
+
+# remove '*' from Type column
+trnL_link_file$Type <- str_replace(trnL_link_file$Type, '[*]', NA_character_)
+
+# write CSV file
+write.csv(trnL_link_file, "./SequencedData/Plants/trnL_taxa_link_file.csv", row.names = FALSE)
+
+#=====================================================================================
+### FECAL DATA ###
+
+trnL_fecal <- trnL_fecal[-(c(452:453)),]
 
 # reads dataframe
 
@@ -44,3 +61,6 @@ reads <- tidyr::gather(reads, "Sample", "Reads", 2:48) %>%
 
 samples <- select(samples, vial_barcode:PIT_tag) %>% 
   rename(Sample = vial_barcode)
+
+#=====================================================================================
+### VOUCHER DATA ###
