2a19b91043d3d127832dd6d3ed420f0472a793fc
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2017-06-03 16:01:25 -0400

    script for making ITS test files

--- SequencedData/Trap_Bait_Test/scripts/test_ITS_data.R
+++ SequencedData/Trap_Bait_Test/scripts/test_ITS_data.R
@@ -21,51 +21,41 @@ plants <- read.csv("./CollectionData/plant_voucher_collection.csv", header = TRU
 ########################
 # CLEAN DATA
 
-# Blast and no blast files
-
-blast <- select(blast, -Sum)
-blast <- blast[-c(463:464),]
-no_blast <- select(no_blast, -Sum)
+# remove Sum column
+ITS <- select(ITS, -Sum)
 
 # Pull out ConsensusLineage
 
-taxa_its <- select(blast, OTU.ID, ConsensusLineage)
+its_test_taxa <- select(ITS, OTU.ID, ConsensusLineage)
 
 for(this_level in c('k','p','c','o','f','g','s')){
   # separate taxa into columns
-  step_one=sapply(strsplit(as.character(taxa_its$ConsensusLineage), paste0(this_level,'__')), '[', 2)
+  step_one=sapply(strsplit(as.character(its_test_taxa$ConsensusLineage), paste0(this_level,'__')), '[', 2)
   step_two=sapply(strsplit(step_one, ';'), '[', 1)
-  taxa_its[,this_level]=step_two
+  its_test_taxa[,this_level]=step_two
 }
 
-taxa_its <- select(taxa_its, OTU.ID, k:s) %>% 
-  distinct(OTU.ID, .keep_all = TRUE) %>% 
+# make taxa link file
+its_test_taxa <- select(its_test_taxa, OTU.ID, k:s) %>% 
   rename(Family = f, Genus = g, SciName = s)
+write.csv(its_test_taxa, "SequencedData/Trap_Bait_Test/ITS_test_taxa_link_file.csv", row.names = FALSE)
 
-#write.csv(taxa_its, "SequencedData/Plants/ITS_BLAST_taxa_link_file.csv", row.names = FALSE)
+# Restructure
 
-# Restructure and combine
+ITS <- select(ITS, -ConsensusLineage)
 
-blast <- select(blast, -ConsensusLineage)
-all_ITS <- bind_rows(blast, no_blast)
-
-all_ITS <- tidyr::gather(all_ITS, "Sample", "Reads", S008809.Wisely:S009099.Wisely) %>%
+ITS_reads <- tidyr::gather(ITS, "Sample", "Reads", 2:98) %>%
   filter(Reads != 0)
-all_ITS <- tidyr::separate(all_ITS, Sample, into = c("Sample", "Wisely")) %>%
+ITS_reads <- tidyr::separate(ITS_reads, Sample, into = c("Sample", "Wisely")) %>%
   select(-Wisely)
 
 # vouchers dataframe
-
 vouchers <- select(plants, vial_barcode, year) %>% 
-  filter(year != '2017') %>% 
   rename(Sample = vial_barcode) %>% 
   select(-year)
-vouchers_its <- semi_join(all_ITS, vouchers, by = "Sample")
-
-#write.csv(vouchers_its, "SequencedData/Plants/ITS_voucher_data.csv", row.names = FALSE)
+vouchers_its <- semi_join(ITS_reads, vouchers, by = "Sample")
+#write.csv(vouchers_its, "SequencedData/Trap_Bait_Test/test_ITS_voucher_data.csv", row.names = FALSE)
 
 # fecal sample dataframe
-
-fecal <- anti_join(all_ITS, vouchers, by = "Sample")
-
-#write.csv(fecal, "SequencedData/Plants/ITS_fecal_data.csv", row.names = F)
+fecal <- anti_join(ITS_reads, vouchers, by = "Sample")
+#write.csv(fecal, "SequencedData/Trap_Bait_Test/test_ITS_fecal_data.csv", row.names = F)
