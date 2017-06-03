d46473cb2a8dca0cf4a36747cbc3937f7ed53784
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2017-06-03 14:50:58 -0400

    add script for processing trnL test data

---
+++ SequencedData/Trap_Bait_Test/trnL_test_data_prep.R
@@ -0,0 +1,47 @@
+# Prep trnL data from Jonah Inc.
+# Ellen Bledsoe
+# Jan 2017
+
+########################
+# LIBRARIES
+
+library(dplyr)
+library(stringr)
+
+########################
+# LOAD FILES
+
+# set your working directory
+setwd("~bleds22e/Documents/Git/Metagenomics/")
+
+# load the files you need
+trnL <- read.csv("./SequencedData/Trap_Bait_Test/trnL_trap_and_bait_test.csv", header = TRUE)
+fecal <- read.csv("./CollectionData/fecal_sample_collection.csv", header = TRUE)
+plants <- read.csv("./CollectionData/plant_voucher_collection.csv", header = TRUE)
+
+########################
+# CLEAN DATA
+
+# taxonomy dataframe
+taxa_trnL <- select(trnL, OTU_ID, ConsensusLineage) %>% 
+  rename(OTU.ID = OTU_ID)
+
+for(this_level in c('d','k','p','c','o','f','g','s')){
+  # separate taxa into columns
+  step_one=sapply(strsplit(as.character(taxa_trnL$ConsensusLineage), paste0(this_level,'__')), '[', 2)
+  step_two=sapply(strsplit(step_one, ';'), '[', 1)
+  taxa_trnL[,this_level]=step_two
+}
+
+taxa_trnL <- rename(taxa_trnL, Family = o, Genus = g, Species = s)
+
+# reads dataframe
+
+reads <- trnL[,c(1,8:54)] %>% rename(OTU.ID = OTU_ID)
+reads <- tidyr::gather(reads, "Sample", "Reads", 2:48) %>% 
+  filter(Reads != 0)
+
+# samples dataframe
+
+samples <- select(samples, vial_barcode:PIT_tag) %>% 
+  rename(Sample = vial_barcode)
