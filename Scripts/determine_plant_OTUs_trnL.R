9179162f3736d8fb0bce66ef2c6872a18bc8eceb
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2020-01-23 00:25:30 -0500

    code outline for going through reads and selecting OTUs that correspond to vouchers

--- Scripts/determine_plant_OTUs_trnL.R
+++ Scripts/determine_plant_OTUs_trnL.R
@@ -16,3 +16,38 @@ trnL_reads <- filter(trnL_reads, SampleID %in% sample_id$vial_id)
 trnL_totals <- filter(trnL_totals, SampleID %in% sample_id$vial_id)
 
 # Make Histograms
+
+voucher_ids <- unique(trnL_reads$SampleID)
+
+OTU_list <- list() # can you plyr::ldply(list, data.frame) to merge into dataframe when done
+passed_list <- list()
+
+for (i in 1:length(voucher_ids)) {
+  
+  id <- voucher_ids[i]
+  id_OTUs <- trnL_reads[(trnL_reads$SampleID == id), ]
+  print(id_OTUs)
+  hist(id_OTUs$Reads)
+  
+  print("Input the number of OTUs to keep [0-9] or type 'pass'")
+  
+  rows.to.keep = readline()
+  if(rows.to.keep == 0) {
+    # put NA in dataframe
+    # ## To remove a star:
+    # ws[which(ws$tag == extrastar[i, 'tag']), 'note2'] <- NA
+    # print(ws[which(ws$tag == extrastar[i, 'tag']), ])
+    # print('Remember to record on datasheet + in notebook!')
+  } else if (rows.to.keep %in% 1:20){
+    # put those OTUs in the dataframe with that sample number (list)
+  } else {
+    # make list of 
+  }
+  readline(prompt = "Press [enter] to continue")
+}
+
+print("You've gone through all the vouchers. Type 'Y' if you want to revisit the ones you passed")
+revisit.passed = readline()
+if (revisit.passed == 'Y') {
+  for (i in 1:length(passed_list))
+}
