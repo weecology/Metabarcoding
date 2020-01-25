4d8b99d46d999bfc0bcf0d6e3d1b05ef1e448828
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2020-01-25 14:30:11 -0500

    add function for reading in ITS files

--- Scripts/functions.R
+++ Scripts/functions.R
@@ -10,4 +10,13 @@ read_in_trnL_files <- function(path = "Data/SequencedData/Plants/RawData/Prepped
   return(data)
 }
 
+read_in_ITS2_files <- function(path = "Data/SequencedData/Plants/RawData/PreppedFiles/ITS2",
+                               files = dir(path, pattern = "*.csv")){
+  # this function reads all CSV files from set path and puts them in a list
+  data <- files %>% 
+    map(~ read_csv(file.path(path, .)))
+  return(data)
+}
+
+
 # convert trnL from reads to proportions #
