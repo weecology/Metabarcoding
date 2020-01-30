ee22e01bafaba28123202cc938eaa7f0db481383
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2020-01-30 09:04:44 -0500

    prep ITS2 files for easy use down the road

--- Scripts/prep_ITS2_files.R
+++ Scripts/prep_ITS2_files.R
@@ -17,18 +17,16 @@ trapandbait <- read_csv("Data/SequencedData/Plants/RawData/ITS2_Trap_Bait_Test.c
 
 # PREP FILES #
 
-# Fall 2017 #
-fall2017 <- fall2017[-(nrow(fall2017)-1),]
-fall2017[nrow(fall2017),1] <- "Total_Reads"
-
-# Millet #
-millet$Sum <- millet$S009063.Wisely
-
-# Reference Samples #
-refsamples$Sum <- rowSums(refsamples[,-c(1:2)])
+# Fall 2017 
+# good
 
 #  Spring 2017 #
+spring2017 <- spring2017[, -c(155:157)]
+
 spring2017$Sum <- rowSums(spring2017[,-c(1:2)])
+
+total_reads_row <- c("Total_Reads", NA, colSums(spring2017[,-c(1:2)]))
+spring2017 <- rbind(spring2017, total_reads_row)
 
 # Spring 2018 #
 total_reads_row <- c("Total_Reads", NA, colSums(spring2018[,-c(1:2)]))
@@ -36,3 +34,25 @@ spring2018 <- rbind(spring2018, total_reads_row)
 
 # Trap & Bait #
 trapandbait$Sum <- rowSums(trapandbait[,-c(1:2)])
+
+# PROCESS FILES #
+
+df_list <- list(fall2017 = fall2017, 
+                spring2017 = spring2017, 
+                spring2018 = spring2018, 
+                trapandbait = trapandbait)
+
+for (i in names(df_list)){
+  
+  # fix naming (i.e., remove Wisely from vial IDs)
+  colnames(df_list[[i]]) <- sub(".Wisely|.Bledsoe|Bledsoe.", "", colnames(df_list[[i]]))
+  colnames(df_list[[i]]) <- sub("[.]2", "", colnames(df_list[[i]]))
+  colnames(df_list[[i]]) <- sub("[.]1", "", colnames(df_list[[i]]))
+  colnames(df_list[[i]])[1] <- "OTU"
+  
+  assign(i, df_list[[i]])
+  
+  write_csv(df_list[[i]], 
+            paste0("Data/SequencedData/Plants/RawData/PreppedFiles/ITS2/", i, ".csv"))
+  
+}
