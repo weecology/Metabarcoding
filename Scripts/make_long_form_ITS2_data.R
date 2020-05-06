23f8d6cb7eca20be44035fc44a01f0ec73198760
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2020-05-06 15:50:41 -0400

    make ITS2 dataframe with Consensus Lineage

--- Scripts/make_long_form_ITS2_data.R
+++ Scripts/make_long_form_ITS2_data.R
@@ -18,9 +18,9 @@ total_list <- list()
 for (i in 1:length(df_list)) {
   
   df <- df_list[[i]]
-  df <- df %>% select(-ConsensusLineage) 
   
-  df_total_reads <- as.vector(df[nrow(df),])
+  df_total_reads <- as.vector(df[nrow(df),]) %>% 
+    select(-ConsensusLineage) 
   df2 <- data.frame(t(df_total_reads[-1]))
   colnames(df2) <- df_total_reads[, 1]
   df2 <- rownames_to_column(df2, var = "SampleID") %>% 
@@ -32,7 +32,7 @@ for (i in 1:length(df_list)) {
     filter(Sum > 0) %>% 
     select(-Sum)
   df <-  df %>% 
-    pivot_longer(cols = c(2:ncol(df)), names_to = "SampleID", values_to = "Reads") %>% 
+    pivot_longer(cols = c(3:ncol(df)), names_to = "SampleID", values_to = "Reads") %>% 
     filter(Reads > 0)
   df$DataFrame <- names(df_list[i])
   reads_list[[i]] <- df
@@ -43,5 +43,5 @@ ITS2_reads <- plyr::ldply(reads_list, data.frame) %>%
   arrange(SampleID, (desc(Reads)))
 ITS2_totals <- plyr::ldply(total_list, data.frame)
 
-write_csv(ITS2_reads, "Data/SequencedData/Plants/ProcessedData/ITS2_reads.csv")
+write_csv(ITS2_reads, "Data/SequencedData/Plants/ProcessedData/ITS2_reads_with_lineage.csv")
 write_csv(ITS2_totals, "Data/SequencedData/Plants/ProcessedData/ITS2_totals.csv")
