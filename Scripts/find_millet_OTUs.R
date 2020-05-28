59aab0be82daedb52343f833c6481426f6e5bb22
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2020-05-27 20:25:48 -0400

    script for finding which OTUs are likely millet and should be removed for both trnL and ITS2
    this script also creates OTU/WTU keys for trnL and ITS2

---
+++ Scripts/find_millet_OTUs.R
@@ -0,0 +1,68 @@
+# Find millet OTUs to remove
+# EKB
+# 27 May 2020
+
+# LIBRARIES #
+library(tidyverse)
+
+### trnL ###
+
+reads_WTU <- read_csv("Data/SequencedData/Plants/ProcessedData/trnL_reads_WeeTU.csv")
+
+# create OTU_WTU key for trnL
+all_OTU_WTU <- reads_WTU %>% 
+  select(OTU, Kingdom:WTU.species) %>% 
+  distinct()
+#write_csv(all_OTU_WTU, "Data/SequencedData/Plants/ProcessedData/trnL_OTU_WTU_key.csv")
+
+# find millet OTUs to remove
+all_species <- reads_WTU %>% 
+  subset(., !duplicated(WTU.species)) %>% 
+  select(Kingdom:WTU.species)
+
+millet_WTU.genus <- all_species %>% 
+  filter(Family == "Poaceae", Subfamily == "Panicoideae" | is.na(Subfamily),
+         Genus == "Panicum" | is.na(Genus)) %>% 
+  select(WTU.genus) %>% 
+  distinct()
+
+millet_OTUs <- reads_WTU %>% 
+  filter(WTU.genus %in% millet_WTU.genus$WTU.genus) %>% 
+  select(OTU) %>% 
+  distinct()
+
+### ITS2 ###
+
+reads_WTU_ITS2 <- read_csv("Data/SequencedData/Plants/ProcessedData/ITS2_reads_WeeTU.csv")
+
+# create OTU_WTU key for trnL
+all_OTU_WTU_ITS2 <- reads_WTU_ITS2 %>% 
+  select(OTU, Domain:WTU.species) %>% 
+  distinct()
+#write_csv(all_OTU_WTU_ITS2, "Data/SequencedData/Plants/ProcessedData/ITS2_OTU_WTU_key.csv")
+
+# find millet OTUs to remove
+all_species_ITS2 <- reads_WTU_ITS2 %>% 
+  subset(., !duplicated(WTU.species)) %>% 
+  select(Domain:WTU.species)
+
+# WTU.species 119:125 are Panicum
+#  - WTU.species.122 is Panicum miliaceum
+#  - WTU.species.121 is Panicum hirticaule
+millet_WTU.sp_ITS2 <- all_species_ITS2 %>% 
+  filter(Family == "Poaceae", Genus == "Panicum" | is.na(Genus), WTU.species != 121) %>% 
+  select(WTU.species) %>% 
+  distinct()
+
+millet_OTUs_ITS2_no.hirt <- reads_WTU_ITS2 %>% 
+  filter(WTU.species %in% millet_WTU.sp_ITS2$WTU.species) %>% 
+  select(OTU) %>% 
+  distinct()
+
+millet_OTUs_ITS2_pani.mili.only <- reads_WTU_ITS2 %>% 
+  filter(WTU.species == 122) %>% 
+  select(OTU) %>% 
+  distinct()
+
+# no.hirt and pani.mili.only are almost identical (6095 OTUs vs. 5984 OTUs)
+# probably best to be conservative, so will go with no.hirt
